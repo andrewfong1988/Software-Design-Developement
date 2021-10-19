268a5120c6bf87c8dbc99f57703390145c8a1031
Andrew Fong <andrew.fong.1988@gmail.com>
2021-10-19 11:30:26 +1100

    commit

---
+++ recursion_1.py
@@ -0,0 +1,14 @@
+# Example 1 - ulgometh (infinite)
+
+# def ulgometh(x):
+#     print(x)
+#     ulgometh(x-1)
+# ulgometh(5)
+
+# Example 2 - ulgometh (fixed)
+
+# def ulgometh(x):
+#     if x > 0:
+#         print(x)
+#         ulgometh(x-1)
+# ulgometh(5)
