21665bcf91950eb7dc6c37eacf62a3f1b22e7871
Andrew Fong <andrew.fong.1988@gmail.com>
2021-10-21 18:49:19 +1100

    commit

---
+++ Challenges/466.py
@@ -0,0 +1,8 @@
+# 466
+# Downloaded from: https://github.com/dwyl/english-words/
+
+f = open('Challenges\words.txt', 'r') # change this to your relative path
+content = f.readlines()
+
+# 1 - Implement binary search that will find the index value of any word in the text file based on user input
+# 2 - 
