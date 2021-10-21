d8ded0ef230b0bc220ed095b50c41cd4fd257dcd
andrewfong1988 <andrew.fong.1988@gmail.com>
2021-10-22 10:05:02 +1100

    commit

--- Challenges/466.py
+++ Challenges/466.py
@@ -4,5 +4,7 @@
 f = open('Challenges\words.txt', 'r') # change this to your relative path
 content = f.readlines()
 
-# 1 - Implement binary search that will find the index value of any word in the text file based on user input
-# 2 - 
+# 0 - Import text file into Python using the code fragment above
+# 1 - Implement a binary search that will find the index value of any word in the text file based on user input
+# 2 - Find the index locations of the words: "Bumfuzzle", "Malarkey", "Slag"
+# 3 - FInd and store/print the number of words that contain the letters "poo" in that order
