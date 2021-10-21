dfa41151fe9bcb8674699b5d9af1cc2158fb117b
Andrew Fong <andrew.fong.1988@gmail.com>
2021-10-21 18:10:50 +1100

    commit

---
+++ Challenges/jungle_book.py
@@ -0,0 +1,9 @@
+# import the Jungle Book by Rudyard Kipling
+# count number of times the following characters are mentioned
+# number of commas
+import os
+print(os.getcwd())
+# with open("jungle_book.txt",'r') as data_file:
+#     for line in data_file:
+#         data = line.split()
+#         print(data)
